edd27158332220a5b95b61b5c18782999f2f98c0
lawson1000 <lawsonolanrewaju@yahoo.com>
2023-06-06 19:46:14 +0100

    commited

--- test.py
+++ test.py
@@ -1,5 +1,60 @@
-colors= "GREEN, YELLOW, GREEN, BROWN, BLUE, PINK, BLUE, YELLOW, ORANGE, CREAM, ORANGE, RED, WHITE, BLUE, WHITE, BLUE, BLUE, BLUE, GREEN"
-colors= colors.split(", ")
-print(colors)
+from random import randint
+
+monday= "GREEN, YELLOW, GREEN, BROWN, BLUE, PINK, BLUE, YELLOW, ORANGE, CREAM, ORANGE, RED, WHITE, BLUE, WHITE, BLUE, BLUE, BLUE, GREEN"
+tuesday = "ARSH, BROWN, GREEN, BROWN, BLUE, BLUE, BLEW, PINK, PINK, ORANGE, ORANGE, RED, WHITE, BLUE, WHITE, WHITE, BLUE, BLUE, BLUE"
+wednesday = "GREEN, YELLOW, GREEN, BROWN, BLUE, PINK, RED, YELLOW, ORANGE, RED, ORANGE, RED, BLUE, BLUE, WHITE, BLUE, BLUE, WHITE, WHITE"
+thursday = "BLUE, BLUE, GREEN, WHITE, BLUE, BROWN, PINK, YELLOW, ORANGE, CREAM, ORANGE, RED, WHITE, BLUE, WHITE, BLUE, BLUE, BLUE, GREEN"
+friday = "GREEN, WHITE, GREEN, BROWN, BLUE, BLUE, BLACK, WHITE, ORANGE, RED, RED, RED, WHITE, BLUE, WHITE, BLUE, BLUE, BLUE, WHITE"
+
+# converting to list
+monday= monday.split(", ")
+tuesday= tuesday.split(", ")
+wednesday= wednesday.split(", ")
+thursday= thursday.split(", ")
+friday= friday.split(", ")
+
+days=[tuesday,wednesday,thursday,friday]
+
+# extending all the colors into one list
+for i in days:
+    monday.extend(i)
+
+newWeek = set(monday)
+ColorCount = {}
+
+# how many times a color occurs
+for i in newWeek:
+    ColorCount[i] = monday.count(i)
+
+print(len(monday))
+# The mean is Blue because it occurs the most
+print(ColorCount)
+
+# 2)
+# The Staff wear Blue most times of the week
 
 
+# 3)
+
+median = sorted(monday)
+print(len(median)/2)
+
+# median
+print(median[48])
+# median is GREEN
+
+
+
+# 6)
+
+# 9)
+ConcatNum=""
+for i in range(4):
+    ab = randint(0, 1)
+    ConcatNum += str(ab)
+
+# 4 digits 0s and 1s
+print(ConcatNum)
+
+# converted to base 10
+print(int(ConcatNum,2))
