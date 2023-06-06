95e1fec3b6fa20d38f0d279c352ab5bded644041
lawson1000 <lawsonolanrewaju@yahoo.com>
2023-06-06 16:17:47 +0100

    first commit

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 colors= "GREEN, YELLOW, GREEN, BROWN, BLUE, PINK, BLUE, YELLOW, ORANGE, CREAM, ORANGE, RED, WHITE, BLUE, WHITE, BLUE, BLUE, BLUE, GREEN"
 colors= colors.split(", ")
 print(colors)
+
+
